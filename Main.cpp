c5a1383109d4df2a5f190699665499a9d160bfbf
OriHoward <ori.howard@gmail.com>
2022-03-13 20:56:25 +0200

    bash grade 100 - main is good

--- Main.cpp
+++ Main.cpp
@@ -7,33 +7,32 @@ using namespace std;
 using namespace ariel;
 
 int main() {
-    // random rug generator
-//    vector<char> symbols(94);
-//    for (int j = 0, i = 33; i < 127; ++i, j++) {
-//        symbols[j] = i;
-//    }
-//    int firstSymbolInd = rand() % 94;
-//    int secondSymbolInd = rand() % 94;
-//    cout << mat(13, 13, symbols[firstSymbolInd], symbols[secondSymbolInd]) << endl;
-//    cout << "````````````````````````````````````````" << endl;
-
-    for (int i = 0; i < 256; ++i) {
-        if (!isprint(i) || isspace(i)) {
-            cout << i << endl;
-        }
+    /**
+     * generates random rug
+     */
+    vector<char> symbols(94);
+    int firstSymbolInd;
+    int secondSymbolInd;
+    int randomCol;
+    int randomRow;
+    for (int j = 0, i = 33; i < 127; ++i, j++) {
+        symbols[j] = i;
     }
-    cout << "Welcome to the rug shop !" << endl;
-    int cols, rows;
-    char firstSymbol, secondSymbol;
-    cout << "Enter two odd numbers for the size of the rug: " << endl;
-    cin >> cols >> rows;
-    cout << "Enter two valid symbols for the rug design: " << endl;
-    cin >> firstSymbol >> secondSymbol;
-    cout << "Here is your rug, thanks for buying!" << endl;
-    cout << "--------------" << endl;
-    cout << mat(cols, rows, '%', '^') << endl;
-    cout << "--------------" << endl;
-    cout << "Random rugs for display" << endl;
+    cout << "------------Generating Random Rugs------------" << endl;
+    srand((time(NULL)));
+    for (int i = 0; i < 10; ++i) {
+        firstSymbolInd = rand() % 94;
+        secondSymbolInd = rand() % 94;
+        randomCol = rand() % 31;
+        randomRow = rand() % 31;
+        randomCol = (randomCol % 2 == 0) ? randomCol + 1 : randomCol;
+        randomRow = (randomRow % 2 == 0) ? randomRow + 1 : randomRow;
+        cout << "--------------------------------------------" << endl;
+        cout << mat(randomCol, randomRow, symbols[firstSymbolInd], symbols[secondSymbolInd]) << endl;
+    }
+    cout << "------------Finished !------------" << endl;
+
+    cout << "Basics rugs for display" << endl;
     cout << "-------------------------------" << endl;
     cout << "Basic rug" << endl;
     cout << "--------------" << endl;
@@ -58,5 +57,15 @@ int main() {
     cout << "Huge sexy rug" << endl;
     cout << "--------------" << endl;
     cout << mat(55, 55, '%', '*') << endl;
-
+    cout << "Welcome to the rug shop !" << endl;
+    int cols, rows;
+    char firstSymbol, secondSymbol;
+    cout << "Enter two odd numbers for the size of the rug: " << endl;
+    cin >> cols >> rows;
+    cout << "Enter two valid symbols for the rug design: " << endl;
+    cin >> firstSymbol >> secondSymbol;
+    cout << "Here is your rug, thanks for buying!" << endl;
+    cout << "--------------" << endl;
+    cout << mat(cols, rows, '%', '^') << endl;
+    cout << "--------------" << endl;
 }
